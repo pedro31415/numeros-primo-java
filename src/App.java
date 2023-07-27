714c36324334321048475a6cd814d8d1f4bda53e
Pedro <pedrogoku80@gmail.com>
2023-07-26 21:41:30 -0300

    Numeros primos

---
+++ src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] primeNumbers = new int[1000];
+        int countCousin = 0;
+        int value = 1;
+        while(countCousin < primeNumbers.length){
+            boolean prime = true;
+            for(int i=2; i<value; i++){
+                if(value%i == 0){
+                    prime = false;
+                    break; 
+                }
+            }
+            if(prime){
+                countCousin++;
+                primeNumbers[countCousin-1] = value;
+                System.out.println("Número primo encontrado: " + value);
+            }
+            value++;
+        }
+    }
+}
